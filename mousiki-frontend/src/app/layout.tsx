bd87d03a0c10e69b1b651a6b049219da5a559686
Turtel216 <papadimitrios216@gmail.com>
2025-07-10 00:19:50 +0300

    Add ported home page

--- mousiki-frontend/src/app/layout.tsx
+++ mousiki-frontend/src/app/layout.tsx
@@ -1,13 +1,5 @@
-// Import styles of packages that you've installed.
-// All packages except `@mantine/hooks` require styles imports
+import { MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
-
-import { ColorSchemeScript, MantineProvider, mantineHtmlProps } from '@mantine/core';
-
-export const metadata = {
-  title: 'My Mantine app',
-  description: 'I have followed setup instructions carefully',
-};
 
 export default function RootLayout({
   children,
@@ -15,12 +7,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" {...mantineHtmlProps}>
-      <head>
-        <ColorSchemeScript />
-      </head>
+    <html lang="en">
       <body>
-        <MantineProvider>{children}</MantineProvider>
+        <MantineProvider>
+          {children}
+        </MantineProvider>
       </body>
     </html>
   );
--- mousiki-frontend/src/app/pages/_app.tsx
+++ mousiki-frontend/src/app/pages/_app.tsx
@@ -1,31 +1,10 @@
-// Import styles of packages that you've installed.
-// All packages except `@mantine/hooks` require styles imports
+import { MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
-
 import type { AppProps } from 'next/app';
-import { createTheme, MantineProvider, MantineColorsTuple } from '@mantine/core';
-
-const myColor: MantineColorsTuple = [
-  '#ffeaec',
-  '#fcd4d7',
-  '#f4a7ac',
-  '#ec777e',
-  '#e64f57',
-  '#e3353f',
-  '#e22732',
-  '#c91a25',
-  '#b41220',
-  '#9e0419'
-];
-const theme = createTheme({
-  colors: {
-    myColor,
-  }
-});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MantineProvider theme={theme}>
+    <MantineProvider>
       <Component {...pageProps} />
     </MantineProvider>
   );
